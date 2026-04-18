00fe7bb1e6a4512059c57fe51258a88d17d5835b
Khushi Prasad <khuprajiv@email.com>
2026-04-18 20:43:48 +0530

    Initial commit - Wild Horizons API

---
+++ utils/getDataByPathParams.js
@@ -0,0 +1,8 @@
+//getDataByPathParams.js
+export const getDataByPathParams = (data, locationType, locationName) => {
+
+  return data.filter((destination) => {
+    return destination[locationType].toLowerCase() === locationName.toLowerCase()
+  })
+
+}
